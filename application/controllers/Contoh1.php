d7d44fabaa625a6cb997067a7637c4e6a79e58f0
Dendi-27 <habilaqila27@gmail.com>
2023-09-29 16:57:55 +0700

    instalasi CI3 dan menyelesaikan Contoh1 etryy
    ;
    '

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalan</h1>";
+        echo"Nama saya Dendi anugrah saya tinggal di daerah bogor olahraga yang saya sukai adalah sepakbola";
+        
+    }
+}
---
+++ application/views/homepage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Homepage</title>
+    <style>
+        body {
+            background-image: url('https://tse1.mm.bing.net/th?id=OIP.fxzn-0tmLQKEu4RxhKoLVQHaEK&pid=Api&P=0&h=220');
+            font-family: Arial, sans-serif;
+            background-color: #000;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+            margin-top: 50px;
+            border-radius: 10px;
+        }
+        h1{
+           color : white;
+        }
+        p{
+            color : white;
+        }
+        h2{
+            color : white;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+    <img src="https://scontent.fcgk4-5.fna.fbcdn.net/v/t39.30808-6/339836610_667763785028738_5398824747927117400_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeGIjejDhs2B2jLIAp9dRVcRCqfmxsX0SHIKp-bGxfRIcj2mavzAUNlk3hThYlLDCGQpeQsDXg1NcvChh062JgH-&_nc_ohc=Iah1p8sDEwYAX9v5Tmp&_nc_zt=23&_nc_ht=scontent.fcgk4-5.fna&_nc_e2o=s&oh=00_AfBZrFaHPhMkK-pg-UUBI4BDhemPRo8GV10Oalwrh8TwmQ&oe=651C6779" alt="Foto Profil" style="width: 150px; height: 150px; border-radius: 50%;">   
+    <h1>Selamat datang di hompage saya !!</h1>
+        <p>Hello, saya Dendi anugrah mahasiswa UBSI yang berdomisili di Bogor,</p>
+        <h2>About Me</h2>
+        <p>Jangan ikuti saya, saya juga ikut-ikutan</p>
+        <h2>Contact</h2>
+        <p>Email: <a href="habilaqila27@gmail.com">habilaqila27@gmail.com</a><br>
+           Phone: <a href="telp:085716057735">085716057735</a></p>
+    </div>
+</body>
+
+</html>
+
